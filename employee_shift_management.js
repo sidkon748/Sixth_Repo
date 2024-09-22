2d5d8366a66e8b24d3782a466967555d92fd290f
Siddhartha Konduru <kondurus@usf.edu>
2024-09-22 10:12:06 -0400

    Create calculateTotalHours function

--- employee_shift_management.js
+++ employee_shift_management.js
@@ -1,14 +1,14 @@
 // Task 1
 // Initialize employees with shift schedules
 const employees = [
-    { name: 'John', shifts: [{ day: 'Monday', hours: 5 }, { day: 'Tuesday', hours: 7 }] },
-    { name: 'David', shifts: [{ day: 'Tuesday', hours: 6 }] },
-    { name: 'Sara', shifts: [{ day: 'Monday', hours: 8 }, { day: 'Friday', hours: 7 }] },
     { name: 'Emily', shifts: [{ day: 'Friday', hours: 7 }] },
     { name: 'Joseph', shifts: [{ day: 'Monday', hours: 4 }, { day: 'Wednesday', hours: 5 }] },
-    { name: 'Cara', shifts: [{ day: 'Wednesday', hours: 7 }, { day: 'Friday', hours: 5 }] },
-    { name: 'Sam', shifts: [{ day: 'Monday', hours: 9 }] },
-    { name: 'Mary', shifts: [{ day: 'Friday', hours: 9 }] }
+    { name: 'Petra', shifts: [{ day: 'Wednesday', hours: 7 }, { day: 'Friday', hours: 5 }] },
+    { name: 'Sammy', shifts: [{ day: 'Monday', hours: 9 }] },
+    { name: 'Mollie', shifts: [{ day: 'Friday', hours: 9 }] },
+    { name: 'Daniel', shifts: [{ day: 'Monday', hours: 4 }, { day: 'Tuesday', hours: 8 }] },
+    { name: 'Elizabeth', shifts: [{ day: 'Tuesday', hours: 5 }, { day: 'Wednesday', hours: 5 }] },
+    { name: 'Peter', shifts: [{ day: 'Friday', hours: 6 }] }
     ];
 
 // Task 2
@@ -26,23 +26,23 @@ displayEmployeeShifts();
 
 // Task 3
 // Create assignShift function
-function assignShift(day, hours, employeeName);{
+function assignShift(day, hours, employeeName){
     const findEmployee = employees.find(emp => emp.name === employeeName);
 
     // Check if the employee exists
-  if (!employee) {
+  if (!employees) {
     console.log(` ${employeeName} not found.`);
     return;
   }
 
   // Check if the employee already has a shift for that day
-  const presentShift = employee.shifts.find(shift => shift.day === day);
+  const presentShift = employees.find(shift => shift.day === day);
   
   if (presentShift) {
     console.log(`${employeeName} already has a shift on ${day}.`);
   } else {
     // Add the new shift
-    employee.shifts.push({ day, hours });
+    employees.push({ day, hours });
     console.log(`${employeeName} has been assigned ${hours} hours on ${day}.`);
   }
 }
@@ -52,4 +52,21 @@ assignShift('John', 'Monday', 6);
 assignShift('Mary', 'Wednesday', 4); 
 assignShift('Cara', 'Tuesday', 5);
 
+// Task 4
+// Create calculateTotalHours function
+function calculateTotalHours(employeeName) {
+  let totalHours = 0;
+
+  for (const employee of employees) {
+    if (employee.name === employeeName) {
+      for (const shift of employee.shifts) {
+        totalHours += shift.hours;
+      }
+    }
+  }
+
+  return totalHours;
 }
+
+console.log(calculateTotalHours('Emily')); 
+console.log(calculateTotalHours('Peter'));  
